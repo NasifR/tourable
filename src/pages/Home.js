c8c3d55f924e3ca8467b8890366d8ae5a0fda457
unknown <nasifjuhayer@gmail.com>
2024-10-27 19:18:49 -0400

    started hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 import React from "react";
-import '../tailwind.css'
+import '../App.css'
+import '../tailwind.css';
+import bg from '../assets/images/home_bg.png';
 
 function Home() {
-
+    const style = {
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed", // keeps the image fixed on scroll
+    };
 
     return (
-        <div>
-            <h1>Explore Events</h1>
-        </div>
-    )
+        <main className="h-screen w-screen" style={style}>
+            <div className="text-white text-center pt-20">Home</div>
+            <div className="text-white text-center pt-10">Hello</div>
+        </main>
+    );
 }
 
 export default Home;
