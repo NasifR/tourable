35a123c2d2aeafacf411fa4f40ec04d3e2644afa
Pretam Chowdhury <Chowdhurypretam@gmail.com>
2024-10-27 11:33:56 -0400

    Updated the navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // App.js
 import React from "react";
+import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,28 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../tailwind.css'
+import { FaSearch, FaUserCircle } from "react-icons/fa";
+import '../tailwind.css';
+import ggProfileIcon from '../logo/gg_profile.png';
+import logo1 from '../logo/Logo-picture.png';
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="max-w-7xl mx-auto flex justify-between">
-        <ul className="flex space-x-4">
+    <nav className="bg-transparent p-4">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        {/* Left section with logo */}
+        <div className="flex items-center space-x-2">
+          <img src={logo1} alt="Tourable Logo" className="h-12 w-12" /> 
+          <span className="text-black font-bold text-lg">Tourable</span>
+        </div>
+
+        {/* Center section with navigation links */}
+        <ul className="flex space-x-8">
           <li>
-            <Link to="/" className="text-white hover:text-gray-400">Home</Link>
+            <Link to="/" className="text-black hover:text-gray-600">Home</Link>
           </li>
           <li>
-            <Link to="/explore" className="text-white hover:text-gray-400">Explore</Link>
+            <Link to="/explore" className="text-black hover:text-gray-600">Explore</Link>
           </li>
           <li>
-            <Link to="/profile" className="text-white hover:text-gray-400">Profile</Link>
+            <Link to="/profile" className="text-black hover:text-gray-600">Profile</Link>
           </li>
           <li>
-            <Link to="/about" className="text-white hover:text-gray-400">About Us</Link>
+            <Link to="/about" className="text-black hover:text-gray-600">About Us</Link>
           </li>
         </ul>
+
+        {/* Right section with search bar and profile icon */}
+        <div className="flex items-center space-x-4">
+          {/* Search bar */}
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-gray-200 rounded-full py-2 pl-8 pr-4 text-black focus:outline-none"
+            />
+            <FaSearch className="absolute top-1/2 left-2 transform -translate-y-1/2 text-black" />
+          </div>
+          {/* Profile icon */}
+          <FaUserCircle className="text-black text-2xl" />
+        </div>
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+
+
